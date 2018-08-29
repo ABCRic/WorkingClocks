727b0efce0b9226dd147eeac3a1a80f72d14f63f
ABCRic <ABCRic@users.noreply.github.com>
2018-08-29 04:23:58 +0100

    Remove unused import

--- src/main/java/me/abcric/bukkit/workingclocks/WorkingClocks.java
+++ src/main/java/me/abcric/bukkit/workingclocks/WorkingClocks.java
@@ -6,7 +6,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class WorkingClocks extends JavaPlugin implements Listener {
